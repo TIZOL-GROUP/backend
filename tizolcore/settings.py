b008b80643c0b960120d058d52b2798dae58d73b
TIZOL-GROUP <tizolofficial@gmail.com>
2025-10-09 10:49:04 +0000

    Description concise des fonctionnalités ajoutées ou des bugs corrigés

--- tizolcore/settings.py
+++ tizolcore/settings.py
@@ -127,6 +127,22 @@ STORAGES = {
     },
 }
 
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',  # <-- VÉRIFIEZ CETTE LIGNE
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
 # -----------------------------------------------------------------------
 # AUTRES CONFIGURATIONS
 # -----------------------------------------------------------------------
